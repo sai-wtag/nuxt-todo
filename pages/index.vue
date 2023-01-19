83ec0361efa4eb77f83d4c8c5abf7953ecc63420
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-01-19 14:03:08 +0600

    feature: vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>Index Page</h1>
+    <p>
+      This is the index page. You can edit this page in <code>pages/index.vue</code>
+    </p>
+    <p>
+      <ul>
+        <li v-for="todo in todos" :key="todo.name">
+          {{ todo.name }}
+        </li>
+      </ul>
+    </p>
+  </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   name: 'IndexPage',
+  computed: {
+    todos () {
+      return this.$store.state.todos.list
+    }
+  },
+  methods: {
+    addTodo (e) {
+      this.$store.commit('todos/add', e.target.value)
+      e.target.value = ''
+    },
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    })
+  }
+
 }
 </script>
