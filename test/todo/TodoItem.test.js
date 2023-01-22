ec3b27ac2a714deec97b90e438b847b61add01b4
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-01-22 17:42:57 +0600

    test: TodoItem.vue tested

---
+++ test/todo/TodoItem.test.js
@@ -0,0 +1,36 @@
+import { shallowMount } from '@vue/test-utils'
+import TodoItem from '@/components/todo/TodoItem.vue'
+
+describe('TodoItem.vue', () => {
+  const wrapper = shallowMount(TodoItem, {
+    propsData: {
+      todo: {
+        id: 1,
+        title: 'Test Todo',
+        completed: false,
+        createdAt: '2020-20-20',
+        completedAt: null,
+      },
+    },
+    mocks: {
+      $moment: (i) => {
+        return {
+          format: (i) => i,
+        }
+      },
+    },
+  })
+  const div = wrapper.find('div').find('div')
+
+  test('3 action buttons exists', () => {
+    const buttons = div.findAll('button')
+    expect(buttons.length).toBe(3)
+  })
+
+  test('button text', () => {
+    const buttons = div.findAll('button')
+    expect(buttons.at(0).text()).toBe('Complete')
+    expect(buttons.at(1).text()).toBe('Edit')
+    expect(buttons.at(2).text()).toBe('Delete')
+  })
+})
