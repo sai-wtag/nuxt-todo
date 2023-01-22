c8ba0631b23d62a2de9a898e654f8a794c795212
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-01-22 11:35:06 +0600

    feature: search and filter button added

---
+++ components/todo/AddTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Add Task</h2>
+    <div class="add-todo__header">
+      <button>Create</button>
+      <FilterOptions />
+    </div>
+  </div>
+</template>
+<script>
+import FilterOptions from './FilterOptions.vue'
+export default {
+  name: 'AddTodo',
+  components: {
+    FilterOptions,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+<style scoped lang="scss">
+.add-todo__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- components/todo/Index.vue
+++ components/todo/Index.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>Hello</div>
+  <div class="container">
+    <header class="header">
+      <h1>Todos</h1>
+      <SearchBar />
+    </header>
+    <AddTodo />
+  </div>
 </template>
 <script>
+import SearchBar from './SearchBar.vue'
+import AddTodo from './AddTodo.vue'
+
 export default {
   name: 'TodoIndex',
+  components: {
+    SearchBar,
+    AddTodo,
+  },
   data() {
     return {}
   },
 }
 </script>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.container {
+  margin: 0 auto;
+  padding: 0 100px;
+}
+</style>
