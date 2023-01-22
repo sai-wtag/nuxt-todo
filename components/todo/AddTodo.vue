fa557717b1d186968ea697869a9d9df60929bf3b
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-01-22 13:00:40 +0600

    feature: basic add task card design

--- components/todo/AddTodo.vue
+++ components/todo/AddTodo.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
+  <div class="add-todo">
     <h2>Add Task</h2>
     <div class="add-todo__header">
       <button>Create</button>
       <FilterOptions />
     </div>
+    <div class="card-container">
+      <AddTodoCard class="card-item" />
+    </div>
   </div>
 </template>
 <script>
 import FilterOptions from './FilterOptions.vue'
+import AddTodoCard from './AddTodoCard.vue'
 export default {
   name: 'AddTodo',
   components: {
     FilterOptions,
+    AddTodoCard,
   },
   data() {
     return {}
@@ -20,9 +25,20 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+.add-todo {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 10px;
+}
 .add-todo__header {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+.card-container {
+  display: grid;
+  gap: 50px 100px;
+  grid-template-columns: auto auto auto;
 }
 </style>
---
+++ components/todo/AddTodoCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="add-todo-card">
+      <div class="add-todo-card__body">
+        <!-- <input
+          type="text"
+          class="add-todo-card__input"
+          placeholder="Add todo"
+        /> -->
+        <textarea class="add-todo-card__input" placeholder="Add Task" />
+      </div>
+      <div class="add-todo-card__footer">
+        <button class="add-todo-card__footer__btn">Add Task</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+$card-padding: 10px;
+.add-todo-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: $card-padding;
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.add-todo-card__footer {
+  display: flex;
+  align-items: center;
+  gap: 10px 10px;
+}
+
+.add-todo-card__input {
+  resize: none;
+  outline: none;
+  font-family: inherit;
+  font-size: 1rem;
+  width: calc(100% - $card-padding);
+  height: 100px;
+}
+</style>
