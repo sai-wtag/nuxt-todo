353c3d71af58e281f30e428293f22b39123e1c37
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-01-23 11:41:04 +0600

    removed: comments from test files

--- test/LandingPage.test.js
+++ test/LandingPage.test.js
@@ -5,7 +5,6 @@ import TodoIndexComponent from '@/components/todo/Index.vue'
 describe('LandingPage', () => {
   test('todo component exists', () => {
     const wrapper = shallowMount(LandingPage)
-    // const todoApp = wrapper.findComponent(TodoIndexComponent)
     expect(wrapper.exists()).toBe(true)
   })
 })
