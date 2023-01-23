e0fbe4187e14a005c13b64693235cb163a0286c8
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-01-23 12:37:37 +0600

    added: validation while adding task

--- components/todo/AddTodoCard.vue
+++ components/todo/AddTodoCard.vue
@@ -6,14 +6,18 @@
           v-model="form.title"
           type="text"
           class="add-todo-card__input"
-          placeholder="Add Task"
+          :placeholder="`Add Task (Min ${validationRules.title.minLength}, Max ${validationRules.title.maxLength} characters)`"
         />
       </div>
+
       <div class="add-todo-card__footer">
         <button type="submit" class="add-todo-card__footer__btn">
           Add Task
         </button>
         <button>Delete</button>
+        <span v-if="errorMessage" class="error-message">{{
+          errorMessage
+        }}</span>
       </div>
     </form>
   </div>
@@ -26,12 +30,44 @@ export default {
       form: {
         title: '',
       },
+      errorMessage: null,
+      validationRules: {
+        title: {
+          required: true,
+          minLength: 5,
+          maxLength: 50,
+        },
+      },
     }
   },
   methods: {
     addTodo() {
+      const errorMessage = this.checkValidation()
+      if (errorMessage) {
+        this.errorMessage = errorMessage
+        return
+      }
       this.$emit('addTodo', this.form)
       this.form.title = ''
+    },
+
+    checkValidation() {
+      const title = this.form.title.trim()
+      const minTitleLength = this.validationRules.title.minLength
+      const maxTitleLength = this.validationRules.title.maxLength
+
+      if (!title) {
+        return 'Please enter a title'
+      }
+
+      if (title.length < minTitleLength) {
+        return `Title should be more than ${minTitleLength} characters`
+      }
+
+      if (title.length > maxTitleLength) {
+        return `Title should be less than ${maxTitleLength} characters`
+      }
+      return null
     },
   },
 }
@@ -59,5 +95,10 @@ form {
   font-size: 1rem;
   width: calc(100% - $card-padding);
   height: 70px;
+}
+
+.error-message {
+  color: red;
+  font-size: 0.8rem;
 }
 </style>
