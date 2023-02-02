42e0ba77dcc788a56a36765110b4f821e43bc3a1
Asiful Islam Sakib <122719191+sai-wtag@users.noreply.github.com>
2023-02-02 23:42:11 +0600

    design: save & add button

--- components/todo/AddTodoCard.vue
+++ components/todo/AddTodoCard.vue
@@ -13,19 +13,18 @@
       </div>
 
       <div class="add-todo-card__footer">
-        <button type="submit">
+        <button class="btn__save" type="submit">
           {{ isTodoEditing ? $t('save') : $t('add') }}
         </button>
-        <button
+        <span
           v-if="isTodoEditing"
-          type="button"
           @click.prevent="submitHandler($event, shouldComplete)"
         >
-          {{ $t('complete') }}
-        </button>
-        <button @click.prevent="deleteCurrentTask">
-          {{ isTodoEditing ? $t('cancel') : $t('delete') }}
-        </button>
+          <CompleteIcon />
+        </span>
+        <span @click.prevent="deleteCurrentTask">
+          <DeleteIcon />
+        </span>
         <div>
           <span v-if="errorMessage" class="error-message">{{
             errorMessage
@@ -39,9 +38,15 @@
 import { mapGetters } from 'vuex'
 import toast from '@/utils/toast'
 
+import CompleteIcon from '@/icons/CompleteIcon'
+import DeleteIcon from '@/icons/DeleteIcon'
+
 export default {
   name: 'AddTodoCard',
-
+  components: {
+    CompleteIcon,
+    DeleteIcon,
+  },
   props: {
     isTodoEditing: {
       type: Boolean,
@@ -157,20 +162,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/css/variables';
+@import '@/assets/css/mixins';
+
 $card-padding: 10px;
-$button-gap: 5px;
+$button-gap: 10px;
+
 form {
-  display: flex;
-  flex-direction: column;
+  @include flex(column);
   height: 100%;
-  gap: $button-gap;
 }
 
 .add-todo-card__footer {
-  display: flex;
-  align-items: center;
+  @include flex(row, nowrap, flex-start, center, $button-gap);
   width: 100%;
-  gap: $button-gap;
 }
 .add-todo-card__input {
   resize: none;
@@ -179,6 +184,17 @@ form {
   font-size: 1rem;
   width: calc(100% - $card-padding);
   height: 70px;
+}
+
+.btn__save {
+  cursor: pointer;
+  border: $border-1;
+  border-radius: 5px;
+  padding: 5px 10px;
+  background-color: $bg-white;
+  font-size: 16px;
+  font-weight: 500;
+  color: #32394b;
 }
 
 .error-message {
