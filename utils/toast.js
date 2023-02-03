076565068bd93fa140b03435c66d63942bc3ad81
Asiful Islam Sakib <122719191+sai-wtag@users.noreply.github.com>
2023-02-03 18:25:48 +0600

    install: toastify.js instead of sweetalert2

--- utils/toast.js
+++ utils/toast.js
@@ -1,20 +1,37 @@
-import Swal from 'sweetalert2'
+import Toastify from 'toastify-js'
+import 'toastify-js/src/toastify.css'
+
+const styleProperties = {
+  color: '#fff',
+  fontSize: '16px',
+  fontWeight: 700,
+  lineHeight: '21.09px',
+  borderRadius: '5px',
+}
+
+const getStyle = (type) => {
+  switch (type) {
+    case 'success':
+      return {
+        background: '#0BC375',
+        ...styleProperties,
+      }
+    case 'error':
+      return {
+        background: '#E85F5F',
+        ...styleProperties,
+      }
+  }
+}
 
 export default function toast(type, message) {
-  const Toast = Swal.mixin({
-    toast: true,
-    position: 'top',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    },
-  })
-
-  Toast.fire({
-    icon: type,
-    title: message,
-  })
+  Toastify({
+    text: message,
+    duration: 3000,
+    close: true,
+    gravity: 'top', // `top` or `bottom`
+    position: 'center', // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: getStyle(type),
+  }).showToast()
 }
