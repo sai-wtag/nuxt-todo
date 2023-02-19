860d6855f18e69f633b40ada30a1b7b32cb5eac3
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-02-19 15:19:07 +0600

    refactor: locale state

--- utils/constants.js
+++ utils/constants.js
@@ -4,3 +4,7 @@ export const INCOMPLETE = 'incomplete'
 
 export const SUCCESS = 'success'
 export const ERROR = 'error'
+
+export const EN = { code: 'en', name: 'English' }
+export const BN = { code: 'bn', name: 'বাংলা' }
+export const ES = { code: 'es', name: 'Español' }
