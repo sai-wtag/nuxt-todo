76b264ef015d77625e34d1bfac0a79a96445d7e9
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-02-19 11:37:26 +0600

    add: splash screen

---
+++ components/todo/utils/SplashScreen.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="splash-screen">
+    <ProjectLogo h="150" w="150" />
+    <span class="project-title">{{ $t('todos') }}</span>
+  </div>
+</template>
+<script>
+import ProjectLogo from '@/icons/ProjectLogo.vue'
+export default {
+  name: 'SplashScreen',
+  components: {
+    ProjectLogo,
+  },
+}
+</script>
+<style lang="scss">
+@import '@/assets/css/variables';
+.splash-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  height: 100vh;
+  width: 100vw;
+  background-color: $bg-primary;
+  animation: fade 2s linear;
+}
+
+.project-title {
+  font-size: 96px;
+  font-weight: 700;
+  color: $text-primary;
+}
+
+@keyframes fade {
+  0%,
+  100% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
+</style>
