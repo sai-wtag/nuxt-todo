578149876b06a047260b5b6dec2404c53bf48d9b
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-03-02 16:55:53 +0600

    refactor: home page

--- test/pages/index.test.js
+++ test/pages/index.test.js
@@ -5,7 +5,7 @@ import HomePage from '@/pages/index.vue'
 import TodoApp from '@/components/todo/Index.vue'
 
 describe('@/pages/index.vue', () => {
-  it('TodoApp component exists', () => {
+  it('@/components/todo/Index.vue exists', () => {
     const wrapper = shallowMount(HomePage)
     const todoAppComponent = wrapper.findComponent(TodoApp)
     expect(todoAppComponent.exists()).toBe(true)
