6091b80940ef0b7ddc3301394b3f045854b41f03
Md Asiful Islam Sakib <asiful.sakib@welldev.io>
2023-03-02 17:13:57 +0600

    refactor: todo index

--- test/components/todo/Index.test.js
+++ test/components/todo/Index.test.js
@@ -4,6 +4,7 @@ import { Store } from 'vuex'
 
 import localVue from '@/test/utils/localVue.js'
 import { todos } from '@/test/mocks/stores/todos.js'
+
 import TodoIndex from '@/components/todo/Index.vue'
 import TodoHeader from '@/components/todo/Header.vue'
 import TodoContainer from '@/components/todo/TodoContainer.vue'
@@ -25,7 +26,7 @@ const wrapperFactory = () => {
 }
 
 describe('@/components/todo/Index.vue', () => {
-  describe('When todo list in not loading', () => {
+  describe('When todo list is not loading', () => {
     it('Do not render @/components/todo/utils/SplashScreen.vue', () => {
       const wrapper = wrapperFactory()
       const splashScreenComponent = wrapper.findComponent(SplashScreen)
